4f25e21cef6ef96f8d575f879fa408e1e7c7d355
Kehinde Kolade <kolexinfos@gmail.com>
2017-03-20 22:09:36 +0100

    Added Biolerplate Functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import { TutorialPage } from '../pages/tutorial/tutorial';
+
+
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    TutorialPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -20,10 +18,8 @@ import { TabsPage } from '../pages/tabs/tabs';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    TutorialPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,41 @@
 import { Component } from '@angular/core';
+import { NavController, LoadingController } from 'ionic-angular';
+import {InAppBrowser} from 'ionic-native';
 
-import { NavController } from 'ionic-angular';
 
+
+
+
+//import {RadioPlayer} from '../../providers/radio-player/radio-player';
+
+declare var window;
+/*
+  Generated class for the HomePage page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  mySlideOptions = {
+    initialSlide: 1,
+    loop: true,
+    autoplay: 2000,
+    pager:true
+  };
+
+  constructor( public navCtrl: NavController, public loadingCtrl: LoadingController) {
 
   }
+
+onSlideChangeStart(event){
+  //console.log("onSlideChangeStart");
+}
+
+
 
 }
